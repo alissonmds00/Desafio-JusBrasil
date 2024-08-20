8c0cfb372019e54db0f5e484b2e736927cdb1c5c
Alisson Matias <alissonmds00@gmaill..com>
2024-08-19 22:42:03 -0300

    refactor: melhorias na estruturação do código

--- desafio/views/consulta.py
+++ desafio/views/consulta.py
@@ -20,61 +20,71 @@ class ConsultaView(APIView):
   """
   def get(self, request, numero_processo=None):
     #Verifica se o número de processo foi fornecido
-    if numero_processo:
-      processo = numero_processo
-    elif 'processo' in request.query_params:
-      processo = request.query_params['processo']
-    else:
+    processo = self._obter_numero_processo(request, numero_processo)
+    if not processo:
       return Response({'message': 'Número do processo não encontrado'}, status=status.HTTP_400_BAD_REQUEST)
 
     #Consulta e verifica se o processo está no banco de dados
     processo_db = self._verificar_processo_db(processo)
-    
     if processo_db.exists():
       return Response(list(processo_db.values()), status=status.HTTP_200_OK)
     
     #Utiliza o crawler para obter os dados do processo
-    resultado_crawler = App(processo).iniciar_consulta()
+    resultado_crawler = self._obter_dados_crawler(processo)
     if not resultado_crawler:
       return Response({'message': 'Processo não encontrado'}, status=status.HTTP_404_NOT_FOUND) 
     
     #Salva os dados do processo no banco de dados
-    self.salvar_processo_no_banco(resultado_crawler, processo)
-    
-    #Consulta outra vez para retornar os processos salvos no banco de dado
+    self._salvar_processo_db(resultado_crawler, processo)
     processo_db = self._verificar_processo_db(processo)
     return Response(list(processo_db.values()), status=status.HTTP_201_CREATED)
+  
   
   def _verificar_processo_db(self, processo):
     """
     Verifica se o processo já existe no banco de dados.
     """
-    processo_formatado = NumeroProcesso(processo).numero
-    processo_db = Processo.objects.filter(numero_do_processo=processo_formatado)
-    print(processo_formatado)
-    return processo_db
+    numero_processo_formatado = NumeroProcesso(processo).numero
+    return Processo.objects.filter(numero_do_processo=numero_processo_formatado)
+  
+  def _obter_numero_processo(self, request, numero_processo):
+    if numero_processo:
+      return numero_processo
+    return request.query_params.get('numero_processo')
+  
+  def _obter_dados_crawler(self, processo):
+    """
+    Inicia o crawler para obter os dados do processo.
+    """
+    return App(processo).iniciar_consulta()
 
+  def _salvar_processo_db(self, dados, numero_processo):
+    """
+    Envia os dados para a criação do processo posteriormente
+    """
+    numero_processo_formatado = NumeroProcesso(numero_processo).numero
+    for grau, dado in enumerate(dados, start=1):
+      self._criar_processo(dado, numero_processo_formatado, grau)
   
   @transaction.atomic
-  def salvar_processo_no_banco(self, dados, numero_do_processo):
-    numero_processo_formatado = NumeroProcesso(numero_do_processo).numero
-    grau = 0
-    for dado in dados:
-      grau += 1
-      try:
-        Processo.objects.create(
-          numero_do_processo=numero_processo_formatado,
-          grau=grau,
-          classe=dado.get('classe', ''),
-          area=dado.get('area', ''),
-          assunto=dado.get('assunto', ''),
-          data_de_distribuicao=dado.get('data_de_distribuicao', '1970-01-01'),
-          juiz=dado.get('juiz', 'Desconhecido'),
-          valor_da_acao=float(dado.get('valor_da_acao', 0))
-        )
-      except Exception as e:
-        transaction.set_rollback(True)
-        raise e
+  def _criar_processo(self, dado, numero_processo_formatado, grau):
+    """
+    Cria um objeto processo que será persistido no banco de dados
+    """
+    try:  
+      Processo.objects.create(
+        numero_do_processo=numero_processo_formatado,
+        grau=grau,
+        classe=dado.get('classe', ''),
+        area=dado.get('area', ''),
+        assunto=dado.get('assunto', ''),
+        data_de_distribuicao=dado.get('data_de_distribuicao', '1970-01-01'),
+        juiz=dado.get('juiz', 'Desconhecido'),
+        valor_da_acao=float(dado.get('valor_da_acao', 0))
+      )
+    except Exception as e:
+      transaction.set_rollback(True)
+      raise e
     
   
     
